486ded01105279e68dcc0b8ceaaca27061e1636a
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2022-12-29 16:03:42 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,21 +3,21 @@ quitMsg = 'quit'
 host = '192.168.0.163'
 port = 5000  # initiate port no above 1024
 myip = socket.gethostbyname(host)
-server_socket = socket.socket()
-server_socket.bind((host, port))
+server = socket.socket()
+server.bind((host, port))
 print(myip)
 connected = False
 done = False
-server_socket.listen(1)  # accept new connection
+server.listen(1)  # accept new connection
 while not done:
     if connected == False:
-        conn, address = server_socket.accept()
+        conn, address = server.accept()
         connected = True
     else:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
-
-    print(data)
+        conn.send('resp'.encode())
+        print(data)
     
         
 conn.close()
